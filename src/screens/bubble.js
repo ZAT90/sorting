278d38a380c8579105d295f27d60177a7994f6ce
Zebel Al Tareq <bdzebel@gmail.com>
2021-01-26 19:55:00 +0800

    bubble sort completed

--- src/screens/bubble.js
+++ src/screens/bubble.js
@@ -12,6 +12,23 @@ const GridWrapper = styled.div`
   grid-auto-rows: minmax(25px, auto);
 `;
 
+const styles = {
+  container: { display: 'flex', flexDirection: 'row' },
+  swapDiv: { display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh', width: '80vh' },
+  swapBtn: { backgroundColor: 'blue', width: 150 },
+  completeMsg: { color: 'black', marginLeft: 100, marginTop: 50 },
+  newGameBtn: { backgroundColor: 'black', width: 150, marginLeft: 200 },
+  consoleDiv: { display: 'flex', flex: 0.4, backgroundColor: 'grey', flexDirection: 'column' },
+  consoleDiv2: { display: 'flex', flexDirection: 'column' },
+  consoleHeader: { fontWeight: 'bold', fontSize: 20, textDecorationLine: 'underline' },
+  nestedForDiv: { display: 'flex', flexDirection: 'row', marginLeft: 10, marginTop: 50 },
+  nestedForConsole: { display: 'flex', flex: 0.3, backgroundColor: 'black', justifyContent: 'center', color: 'white', flexDirection: 'column', paddingLeft: 10 },
+  nestedForText: { display: 'flex', flex: 0.7, marginLeft: 10, alignItems: 'center', },
+  ifDiv: { display: 'flex', flexDirection: 'row', marginLeft: 10, marginBottom: 30 },
+  ifConsole: { display: 'flex', flex: 0.3, backgroundColor: 'red', justifyContent: 'center', color: 'white' },
+  ifText: { display: 'flex', flex: 0.7, marginLeft: 10 },
+}
+
 export function Bubble() {
   const [isLevel, setIsLevel] = useState(false);
   const [initList, setInitList] = useState([]);
@@ -89,16 +106,16 @@ export function Bubble() {
     setScore(0);
     setNumToCompare(0);
     setHighlightFirst(0);
-    setHighlightSecond(0);
+    setHighlightSecond(1);
     setShow(false);
   }
-  console.log('checking on state reuse: ' + firstList);
+  console.log('checking on state reuse: ' + firstList.length);
   console.log('num to compare: ' + numToCompare);
   console.log('showConsole: ' + showConsole);
   return (
     <div>
       <NavigationBar showConsole={(show) => setShowConsole(show)} />
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div style={styles.container}>
 
         <div style={{ flex: 0.6 }}>
           <h2 style={{ color: 'black', marginLeft: 100 }}>score: {score}</h2>
@@ -113,16 +130,16 @@ export function Bubble() {
             <GridWrapper>
               {firstList.length > 0 && listItems()}
             </GridWrapper>
-            {numToCompare > 1 && <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh', width: '80vh' }}>
-              <Button onClick={() => swapNumbers()} style={{ backgroundColor: 'blue', width: 150 }} >Swap</Button>
+            {numToCompare > 1 && <div style={styles.swapDiv}>
+              <Button onClick={() => swapNumbers()} style={styles.swapBtn} >Swap</Button>
           &nbsp;&nbsp;&nbsp;
-         <Button onClick={() => noSwapNumbers()} style={{ backgroundColor: 'blue', width: 150 }} >No Swap</Button>
+         <Button onClick={() => noSwapNumbers()} style={styles.swapBtn} >No Swap</Button>
             </div>}
 
           </div>}
           {numToCompare === 1 &&
-            <div><h2 style={{ color: 'black', marginLeft: 100, marginTop: 50 }}>Well done! You have completed the game</h2>
-              <Button onClick={() => reStart()} style={{ backgroundColor: 'black', width: 150, marginLeft: 200 }} >Start new Game</Button>
+            <div><h2 style={styles.completeMsg}>Well done! You have completed the game</h2>
+              <Button onClick={() => reStart()} style={styles.newGameBtn} >Start new Game</Button>
             </div>
           }
           <Modal show={show}>
@@ -137,14 +154,41 @@ export function Bubble() {
             </Modal.Footer>
           </Modal>
         </div>
-        {showConsole && <div style={{ display: 'flex', flex: 0.4, backgroundColor: 'grey', flexDirection: 'column' }}>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <span style={{ fontWeight: 'bold', fontSize: 20, textDecorationLine: 'underline' }}>Console</span>
+        {showConsole && firstList.length > 0 && <div style={styles.consoleDiv}>
+          <div style={styles.consoleDiv2}>
+            <span style={styles.consoleHeader}>Console</span>
             <span style={{ marginLeft: 10 }}>array = [ {initList.map((item, i, arr) => <span>{item} {i != (arr.length - 1) ? ',' : ''}</span>)}]</span>
+            {firstList.length - numToCompare > 0 && <span style={{ marginLeft: 10 }}>size = len(array)</span>}
           </div>
-          <div style={{ display: 'flex', flexDirection: 'row', marginLeft: 10, marginTop: 50,  }}>
-            <div style={{ display: 'flex', flex: 0.3, backgroundColor: 'red', justifyContent: 'center',color: 'white' }}>j ={highlightFirst}<br />{returnString()}</div>
-            <div style={{ display: 'flex', flex: 0.7, marginLeft: 10 }}>if array[j] &gt; array [j+1] :<br />
+          {firstList.length - numToCompare > 2 && <div style={styles.nestedForDiv}>
+            <div style={styles.nestedForConsole}>
+
+              step -&gt; 0....{firstList.length}-1
+              <br />
+              <span style={{ marginLeft: 10 }}>
+                {returnNestedRange(firstList.length).map((item, i, arr) => <span style={{ color: firstList.length - numToCompare === i ? 'yellow' : 'white' }}>{item} {i != (arr.length - 1) ? ',' : ''}</span>)}
+              </span>
+            </div>
+
+            <div style={styles.nestedForText}>for step in range of (0, size - 1) :
+            </div>
+          </div>}
+          {firstList.length - numToCompare > 0 && <div style={styles.nestedForDiv}>
+            <div style={styles.nestedForConsole}>
+
+              j -&gt; 0....{firstList.length}-{firstList.length - numToCompare}-1
+              <br />
+              <span style={{ marginLeft: 10 }}>
+                {returnNestedRange(numToCompare).map((item, i, arr) => <span>{item} {i != (arr.length - 1) ? ',' : ''}</span>)}
+              </span>
+            </div>
+
+            <div style={styles.nestedForText}>for j in range of (0, size - step - 1) :
+            </div>
+          </div>}
+          <div style={styles.ifDiv}>
+            <div style={styles.ifConsole}>j ={highlightFirst}<br />{returnString()}</div>
+            <div style={styles.ifText}>if array[j] &gt; array [j+1] :<br />
             array[j],array[j+1] = array[j+1],array[j]
             </div>
           </div>
@@ -153,8 +197,14 @@ export function Bubble() {
     </div>
   )
 
-  function returnString(){
-    if(firstList[highlightFirst] > firstList[highlightSecond]){
+  function returnNestedRange(nums) {
+    let arr = [...Array(nums).keys()];
+    arr.pop();
+    return arr;
+  }
+
+  function returnString() {
+    if (firstList[highlightFirst] > firstList[highlightSecond]) {
       return `${firstList[highlightFirst]},${firstList[highlightSecond]} =${firstList[highlightSecond]},${firstList[highlightFirst]}`
     }
     return 'no changes';
