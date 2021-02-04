ee3d87d928b4542c64b58dc75f379662ce46d803
Zebel Al Tareq <bdzebel@gmail.com>
2021-02-04 22:46:05 +0800

    divide done, tomorrow merge

--- src/screens/merge.js
+++ src/screens/merge.js
@@ -2,9 +2,20 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Button, Modal } from 'react-bootstrap';
 import { NavigationBar } from "../components/NavigationBar";
+import Popup from 'reactjs-popup';
+import pipe from './pipe.jpeg';
 const GridWrapper = styled.div`
   display: grid;
   grid-gap: 10px;
+  margin-top: 1em;
+  margin-left: 6em;
+  margin-right: 6em;
+  grid-template-columns: repeat(12, 1fr);
+  grid-auto-rows: minmax(25px, auto);
+`;
+const GridWrapper2 = styled.div`
+  display: grid;
+  grid-gap: 15px;
   margin-top: 1em;
   margin-left: 6em;
   margin-right: 6em;
@@ -23,23 +34,31 @@ const styles = {
   consoleDiv2: { display: 'flex', flexDirection: 'column' },
   consoleHeader: { fontWeight: 'bold', fontSize: 20, textDecorationLine: 'underline' },
   nestedForDiv: { display: 'flex', flexDirection: 'row', marginLeft: 10, marginTop: 20 },
-  nestedForConsole: { display: 'flex', flex: 0.5, backgroundColor: 'black', justifyContent: 'center', color: 'white',  },
+  nestedForConsole: { display: 'flex', flex: 0.5, backgroundColor: 'black', justifyContent: 'center', color: 'white', },
   nestedForText: { display: 'flex', flex: 0.5, marginLeft: 10, alignItems: 'center', },
   ifDiv: { display: 'flex', flexDirection: 'row', marginLeft: 10, marginBottom: 30 },
   ifConsole: { display: 'flex', flex: 0.5, backgroundColor: 'red', justifyContent: 'center', color: 'white' },
   ifText: { display: 'flex', flex: 0.5, marginLeft: 10 },
 }
-export function Merge (){
+export function Merge() {
   const [isLevel, setIsLevel] = useState(false);
   const [levelName, setLevelName] = useState('');
   const [isFirstListSet, setIsFirstListSet] = useState(false);
+  const [breakDialog, setBreakDialog] = useState(false);
+  const [breakNum, setBreakNum] = useState(null);
   const [initList, setInitList] = useState([]);
   const [firstList, setFirstList] = useState([]);
+  const [mainLeft, setMainLeft] = useState([]);
+  const [mainRight, setMainRight] = useState([]);
+  const [nestedLeft, setNestedLeft] = useState({ left: {}, right: {} });
+  const [nestedRight, setNestedRight] = useState({});
+  const [isLeft, setIsLeft] = useState(false);
+  const [isRight, setIsRight] = useState(false);
+  const [nestedStep, setNestedStep] = useState(0);
   const [score, setScore] = useState(0);
   const [numToCompare, setNumToCompare] = useState(0);
-  const [highlightFirst, setHighlightFirst] = useState(0);
-  const [highlightSecond, setHighlightSecond] = useState(1);
   const [show, setShow] = useState(false);
+  const [nestBreakDialog, setNestBreakDialog] = useState(false);
   const [showConsole, setShowConsole] = useState(false);
   const [chooseNums, setChooseNums] = useState([]);
 
@@ -89,7 +108,24 @@ export function Merge (){
     return (
       firstList.map((number, i) =>
         <Button
-          style={{ backgroundColor:  'green' }} key={i} >{number}</Button>
+          style={{ backgroundColor: 'green' }} key={i} >{number}</Button>
+      )
+    )
+  }
+
+  function mainDivided(list, showGreen) {
+    return (
+      list.map((number, i) =>
+        <Button
+          style={{ backgroundColor: !showGreen ? 'grey' : 'green' }} key={i} >{number}</Button>
+      )
+    )
+  }
+  function listItemsDivided(list, showGreen) {
+    return (
+      list.map((number, i) =>
+        <div style={{ marginLeft: 20, marginTop: 20 }}><Button
+          style={{ backgroundColor: !showGreen ? 'grey' : 'green' }} key={i} >{number}</Button></div>
       )
     )
   }
@@ -111,6 +147,7 @@ export function Merge (){
         alert("easy level has to select 6 numbers");
       } else {
         setIsFirstListSet(true);
+        setBreakDialog(true);
         setNumToCompare(firstList.length);
       }
     } else if (levelName === 'medium') {
@@ -118,6 +155,7 @@ export function Merge (){
         alert("medium level has to select 8 numbers");
       } else {
         setIsFirstListSet(true);
+        setBreakDialog(true);
         setNumToCompare(firstList.length);
       }
     } else if (levelName === 'hard') {
@@ -125,11 +163,14 @@ export function Merge (){
         alert("hard level has to select 10 numbers");
       } else {
         setIsFirstListSet(true);
+        setBreakDialog(true);
         setNumToCompare(firstList.length);
       }
     }
 
   }
+  console.log(nestedLeft);
+  //nestedLeft.map((item, i) => console.log('item: ' + item));
   return (
     <div>
       <NavigationBar showConsole={(show) => setShowConsole(show)} />
@@ -151,19 +192,186 @@ export function Merge (){
 
             </GridWrapper>}
             <GridWrapper>
-              {isFirstListSet && listItems()}
+              {isFirstListSet && mainLeft.length === 0 && listItems()}
+              {mainLeft.length > 0 && mainDivided(mainLeft, isLeft)}
+              {mainLeft.length > 0 && <img src={pipe} width='50' />}
+              {mainLeft.length > 0 && mainDivided(mainRight, isRight)}
+
             </GridWrapper>
+            <div style={{ display: 'flex', flexDirection: 'row', marginLeft: 100 }}>
+              <div>
+                {isLeft &&
+                  Object.keys(nestedLeft.left).map(keyleft =>
+                    <div><span style={{ color: 'black', display: 'flex', flexDirection: 'row' }}>{listItemsDivided(nestedLeft.left[keyleft], true)}</span></div>
+                  )
+
+
+                }
+
+              </div>
+
+              <div >
+                {isLeft &&
+                  Object.keys(nestedLeft.right).map(keyright =>
+                    <div><span style={{ color: 'black', display: 'flex', flexDirection: 'row' }}>
+                     <img src={pipe} width='50' />
+                      {listItemsDivided(nestedLeft.right[keyright], true)}</span></div>
+                  )
+
+
+                }
+              </div>
+            </div>
+            {/* <GridWrapper>
+            {isLeft &&
+              Object.keys(nestedLeft.left).map(keyleft =>
+                <GridWrapper2><span style={{ color: 'black', display: 'flex', flexDirection: 'row' }}>{listItemsDivided(nestedLeft.left[keyleft], true)}</span></GridWrapper2>
+              )
+
+
+            }
+            </GridWrapper> */}
+
             {!isFirstListSet && <div style={styles.swapDiv}>
               <Button onClick={() => startSorting()} style={styles.startBtn} >Start Sorting</Button>
             </div>}
-            
+
 
           </div>}
-          
-          
+          <Popup open={breakDialog} closeOnDocumentClick={false} modal>
+            <div style={{ backgroundColor: 'grey', color: 'white', padding: 20 }}>
+              What is the break point among the numbers
+              <form onSubmit={(event) => formSubmit(event)}>
+
+                <input type="number" value={breakNum != null ? breakNum : 0} onChange={(e) => setBreakNumber(e)} />
+
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+          </Popup>
+          <Popup open={nestBreakDialog} closeOnDocumentClick={false} modal>
+            <div style={{ backgroundColor: 'grey', color: 'white', padding: 20 }}>
+              What is the break point among the numbers
+              <form onSubmit={(event) => innerDivision(event)}>
+
+                <input type="number" value={breakNum != null ? breakNum : 0} onChange={(e) => setBreakNumber(e)} />
+
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+          </Popup>
+          <Modal show={show}>
+            <Modal.Header closeButton>
+              <Modal.Title>Wrong Move</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Well Done! Let's start with left side first</Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={() => {
+                setShow(false);
+                setNestBreakDialog(true);
+              }}>
+                OK
+          </Button>
+            </Modal.Footer>
+          </Modal>
+
         </div>
       </div>
     </div>
   )
+  function setBreakNumber(event) {
+    console.log(event.target.value);
+    setBreakNum(parseInt(event.target.value));
+  }
+
+  function innerDivision(event) {
+    //console.log(Object.keys(nestedLeft));
+    // setNestedLeft({ ...nestedLeft, [nestedStep]: left });
+    if (Object.keys(nestedLeft.left).length > 0) {
+      event.preventDefault();
+      let test = Object.keys(nestedLeft.left).map(key => { return nestedLeft.left[key] });
+      console.log('test: ' + test.length);
+      let listToBreak = test[nestedStep - 1];
+      console.log('listToBreak: ' + listToBreak);
+      console.log('test0 length' + listToBreak.length);
+      if (listToBreak.length > 1) {
+        let mid = Math.floor(test[0].length / 2);
+        console.log('mid: ' + mid);
+        let leftArr = test[0].slice(0, mid)
+        let rightArr = test[0].slice(mid);
+        console.log('left: ' + leftArr);
+        console.log('right: ' + rightArr);
+        setNestedLeft((prevState) => ({ ...prevState, left: { ...prevState.left, [nestedStep]: leftArr } }));
+        setNestedLeft((prevState) => ({ ...prevState, right: { ...prevState.right, [nestedStep]: rightArr } }));
+        // //setNestedLeft({ ...nestedLeft, left: {[nestedStep]: leftArr}});
+        setNestedStep(nestedStep + 1);
+        if(leftArr.length == 1 && rightArr.length == 1){
+         setBreakDialog(false);
+        }
+      }else{
+        let test = Object.keys(nestedLeft.right).map(key => { return nestedLeft.right[key] });
+      console.log('test: ' + test.length);
+      let listToBreak = test[nestedStep - 1];
+      console.log('listToBreak: ' + listToBreak);
+      console.log('test0 length' + listToBreak.length);
+      if (listToBreak.length > 1) {
+        let mid = Math.floor(test[0].length / 2);
+        console.log('mid: ' + mid);
+        let leftArr = test[0].slice(0, mid)
+        let rightArr = test[0].slice(mid);
+        console.log('left: ' + leftArr);
+        console.log('right: ' + rightArr);
+        setNestedLeft((prevState) => ({ ...prevState, left: { ...prevState.left, [nestedStep]: leftArr } }));
+        setNestedLeft((prevState) => ({ ...prevState, right: { ...prevState.right, [nestedStep]: rightArr } }));
+        // //setNestedLeft({ ...nestedLeft, left: {[nestedStep]: leftArr}});
+        setNestedStep(nestedStep + 1);
+        if(leftArr.length == 1 && rightArr.length == 1){
+         setBreakDialog(false);
+        }
+      }
+    }
+      // let mid = Math.floor(listToBreak.length / 2);
+      // console.log('mid: ' + listToBreak[mid])
+    } else {
+      console.log('no objects');
+      let mid = Math.floor(mainLeft.length / 2);
+      let leftArr = mainLeft.slice(0, mid)
+      let rightArr = mainLeft.slice(mid);
+      console.log('left: ' + leftArr);
+      console.log('right: ' + rightArr);
+      setNestedLeft((prevState) => ({ ...prevState, left: { ...prevState.left, [nestedStep]: leftArr } }));
+      setNestedLeft((prevState) => ({ ...prevState, right: { ...prevState.right, [nestedStep]: rightArr } }));
+      //setNestedLeft({ ...nestedLeft, left: {[nestedStep]: leftArr}});
+      setNestedStep(nestedStep + 1)
+    }
+
+    event.preventDefault();
+  }
+
+  function formSubmit(event) {
+    let mid = Math.floor(firstList.length / 2);
+    console.log('mic: ' + mid);
+    if (firstList[mid] === breakNum) {
+      console.log('this is correct');
+      setScore(score + 1)
+      let left = firstList.slice(0, mid)
+      let right = firstList.slice(mid);
+      if (mainLeft.length === 0) {
+        setMainLeft(left);
+        setMainRight(right);
+        setBreakDialog(false);
+        setShow(true);
+        setIsLeft(true);
+        //setNestedStep(nestedStep + 1);
+
+      }
+
+    } else {
+      alert('wrong answer! please input correct answer');
+      setScore(score - 1);
+    }
+
+    event.preventDefault();
+  }
 }
-  
+
